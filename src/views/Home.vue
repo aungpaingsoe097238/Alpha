2cb3c881ffc60afce09ccac16f5bc1a12dcbf022
Aung Paing Soe <alpha.dev.myanmar@gmail.com>
2022-05-12 01:55:51 +0630

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h4>
+      This is the Home View
+    </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
