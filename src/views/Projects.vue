6cee93b18ed8bbedac83738ba92c1f6fe8b90547
Aung Paing Soe <alpha.dev.myanmar@gmail.com>
2022-05-13 13:54:31 +0630

    service

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,15 +1,219 @@
 <template>
-  <div>
-    <h4>This is Projects Page</h4>
+  <div class="d-flex min-vh-100 justify-content-center align-items-center">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <h2>My Projects</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque, commodi consequuntur debitis dolorem dolores
+            doloribus dolorum eligendi eum exercitationem inventore natus non officiis optio quo recusandae sunt suscipit ullam.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12" >
+            <div class="card-container" id="demo"  >
+              <div class="cardww " tabindex="0" v-for="(d,index) in data" :key="index">
+                <h2>{{ d.title }}</h2>
+                <img :src="d.img" width="100%">
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Projects"
+  name: "Projects",
+  data() {
+    return {
+      data : [
+        {
+          'title' : 'Project One',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Two',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Three',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Four',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Five',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Six',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project One',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Two',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Three',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Four',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Five',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Six',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project One',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Two',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Three',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Four',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Five',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        },
+        {
+          'title' : 'Project Six',
+          'img'   : 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/business_shop_qw5t.svg'
+        }
+
+      ]
+    }
+  },
 }
 </script>
 
 <style scoped>
+.wwww {
+  background: rgba(87, 172, 115, 1);
+  background: -moz-linear-gradient(
+      -45deg,
+      rgba(87, 172, 115, 1) 0%,
+      rgba(21, 151, 207, 1) 100%
+  );
+  background: -webkit-gradient(
+      left top,
+      right bottom,
+      color-stop(0%, rgba(87, 172, 115, 1)),
+      color-stop(100%, rgba(21, 151, 207, 1))
+  );
+  background: -webkit-linear-gradient(
+      -45deg,
+      rgba(87, 172, 115, 1) 0%,
+      rgba(21, 151, 207, 1) 100%
+  );
+  background: -o-linear-gradient(
+      -45deg,
+      rgba(87, 172, 115, 1) 0%,
+      rgba(21, 151, 207, 1) 100%
+  );
+  background: -ms-linear-gradient(
+      -45deg,
+      rgba(87, 172, 115, 1) 0%,
+      rgba(21, 151, 207, 1) 100%
+  );
+  background: linear-gradient(
+      135deg,
+      rgba(87, 172, 115, 1) 0%,
+      rgba(21, 151, 207, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(
+      startColorstr="#57ac73",
+      endColorstr="#1597cf",
+      GradientType=1
+  );
+  overflow-x: hidden;
+  font-family: sans-serif;
+  padding: 70px;
+}
+.card-container {
+  display: flex;
+  padding: 2rem 1rem;
+}
+.card-container .cardww {
+  flex: 1;
+  transition: all 0.3s ease-in-out;
+  background-color: #fff;
+  padding: 15px;
+  border-radius: 10px;
+  box-shadow: 0 0 15px 0px rgba(0, 0, 0, 0.2);
+}
+.cardww h2 {
+  font-size: 22px;
+}
+.card-container .cardww:not(:first-child) {
+  margin-left: -10rem;
+}
+.cardww img {
+  padding: 35px;
+  width: 70%;
+  display: block;
+  margin: 0 auto;
+}
+.card-container .cardww:hover,
+.card-container .cardww:focus {
+  transform: translateY(-1rem);
+  outline: none;
+}
+.card-container .cardww:hover ~ .cardww,
+.card-container .cardww:focus ~ .cardww {
+  transform: translateX(11rem);
+}
+
+@media (max-width: 767px) {
+  #demo {
+    margin-top: 0px;
+  }
+
+  .card-container {
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 0rem 1rem 2rem;
+  }
+  .card-container .cardww {
+    flex: 1 1 100%;
+  }
+  .card-container .cardww:not(:first-child) {
+    margin-left: 0rem;
+    margin-top: 2rem;
+  }
+  .card-container .cardww:hover,
+  .card-container .cardww:focus {
+    transform: translateX(0rem);
+  }
+  .card-container .cardww:hover ~ .cardww,
+  .card-container .cardww:focus ~ .cardww {
+    transform: translateY(0rem);
+  }
+  .cardww img {
+    padding: 5px;
+  }
+}
 
 </style>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,8 +1,67 @@
 <template>
-  <div>
-    <h4>
-      This is the Service Page
-    </h4>
+  <div class="d-flex min-vh-100 justify-content-center align-items-center">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <h2>What is my Services</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eius ut.
+            Aliquam magni maiores mollitia saepe sed? Adipisci corporis, dolor ex
+            laborum nam nisi placeat totam veniam! Officia quo, voluptate.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-4">
+          <div class="card ">
+            <div class="face face1">
+              <div class="content">
+                <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true">
+                <h3>Design</h3>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                <a href="#" class="btn btn-outline-dark btn-sm mt-2 form-control">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="card ">
+            <div class="face face1">
+              <div class="content">
+                <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true">
+                <h3>Design</h3>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                <a href="#" class="btn btn-outline-dark btn-sm mt-2 form-control">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="card ">
+            <div class="face face1">
+              <div class="content">
+                <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true">
+                <h3>Design</h3>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                <a href="#" class="btn btn-outline-dark btn-sm mt-2 form-control">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,5 +72,72 @@ export default {
 </script>
 
 <style scoped>
+ .card{
+  position: relative;
+  cursor: pointer;
+  border: 0;
+}
+
+.card .face{
+  width: 300px;
+  height: 200px;
+  transition: 0.5s;
+  border-radius: 15px;
+}
+
+.card .face.face1{
+  position: relative;
+  background: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  transform: translateY(100px);
+}
+
+.card:hover .face.face1{
+  background: #ff0057;
+  transform: translateY(0);
+}
+
+.card .face.face1 .content{
+  opacity: 0.2;
+  transition: 0.5s;
+}
+
+.card:hover .face.face1 .content{
+  opacity: 1;
+}
+
+.card .face.face1 .content img{
+  max-width: 100px;
+}
+
+.card .face.face1 .content h3{
+  margin: 10px 0 0;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+  font-size: 1.5em;
+}
+
+.card .face.face2{
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+  transform: translateY(-100px);
+}
+
+.card:hover .face.face2{
+  transform: translateY(0);
+}
+
+.card .face.face2 .content p{
+  margin: 0;
+  padding: 0;
+}
 
 </style>
